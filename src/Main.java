c38f408c8206dcff35d41eb8c0dd73b6c412f945
VasaPopkin <nmichkarev@mail.ru>
2014-03-09 13:57:04 +0900

    Initial commit AI added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args){
 
-        int i,size, maxCountOfSteps, line, row, menuLine;
+        int i,size, maxCountOfSteps, menuLine;
         boolean gmr = true;
         int[] cellCoords = new int[2];
 
